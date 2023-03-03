0b251d135c166b3ef2aa9bff286378fb13e3900a
emanuele sacchetti <manusacco92@gmail.com>
2023-03-03 16:58:57 +0100

    creata la grigia. ora focus sul pulsante di creazione della griglia stessa

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 const bottonePlay = document.querySelector('.btn');
-const gridDom = document.getElementById('grid');
+const containerDom = document.querySelector('.container');
 
 let numeroQuadrati = 100;
 
-for (let i = 0; i < numeroQuadrati; i++){
-    const square = newSquare(); 
-    square.append(generateNumbers(1, 100));
+bottonePlay.addEventListener('click', 
+    function (){
+        
+        
+            const gridDom = document.getElementById('grid');
 
-    square.addEventListener('click', 
-        function(){
-            this.classList.toggle('active');
+            for (let i = 0; i < numeroQuadrati; i++){
+                const square = newSquare(); 
+                //square.append(i);
+            
+                square.addEventListener('click', 
+                    function(){
+                        this.classList.toggle('active');
+                        console.log(i);
+                    }
+                )
+                gridDom.append(square);
+            }
         }
-    )
     
-    
-    gridDom.append(square);
-}
-
-
+)
 
 function newSquare() {
     const quadratino = document.createElement('div');
     quadratino.classList.add('square');
     return quadratino;
-}
-
-function generateNumbers(min, max){
-    for( let i = min; i <= max; i++){
-        return i;
-    }
 }
