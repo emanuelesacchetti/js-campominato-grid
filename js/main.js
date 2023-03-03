c6c36dedff2d454d33ee84189595611fdf01e281
emanuele sacchetti <manusacco92@gmail.com>
2023-03-03 17:36:59 +0100

    ultimo aggior

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 const bottonePlay = document.querySelector('.btn');
+
 const containerDom = document.querySelector('.container');
 
 let numeroQuadrati = 100;
 
 bottonePlay.addEventListener('click', 
     function (){
-        
-        
+            
             const gridDom = document.getElementById('grid');
-
+            gridDom.innerHTML = (gridDom);
+            
             for (let i = 0; i < numeroQuadrati; i++){
                 const square = newSquare(); 
-                //square.append(i);
+                
             
                 square.addEventListener('click', 
                     function(){
                         this.classList.toggle('active');
+                        square.append(i);
                         console.log(i);
                     }
                 )
                 gridDom.append(square);
             }
         }
-    
 )
 
 function newSquare() {
